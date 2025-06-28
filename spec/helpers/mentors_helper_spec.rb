4f97be64b93bdd3076297e135852b99088591b71
lorena.marinho <lorena.marinho@rdstation.com>
2025-06-28 14:34:07 -0300

    add comment

--- spec/helpers/mentors_helper_spec.rb
+++ spec/helpers/mentors_helper_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe MentorsHelper, type: :helper do
   context "formatting mentorship topics" do
     it 'returns an empty string when given nil' do
+      # oi, eu sou um comment
       expect(helper.format_mentorship_topics(nil)).to eq("")
     end
 
